bffe0cd19ed5d4cc2c0d49aec855781db137cb3d
merve-bulut <mervebult6@gmail.com>
2026-08-20 19:01:27 +0300

    Check valid answer

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -59,9 +59,43 @@ class ViewController: UITableViewController {
     }
     
     func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    
+                    let IndexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [IndexPath], with: .automatic)
+                }
+            }
+        }
         
     }
     
+    func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
     
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
 }
 
